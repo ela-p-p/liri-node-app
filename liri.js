53084f157faeea19630ad36e756f902768833d2f
Elizabeth P <ejporter9@hotmail.com>
2018-06-28 23:29:54 -0400

    spotify and twitter complete

--- liri.js
+++ liri.js
@@ -1,30 +1,55 @@
 // add code to read and set any environment variables with the dotenv package:
 require("dotenv").config();
-var request = require("request");
+const Twitter = require("Twitter")
+const Spotify = require('node-spotify-api');
+const keys = require("./keys");
+const client = new Twitter(keys.twitter);
+const spotifyQuery = new Spotify(keys.spotify);
+
+const request = require("request");
 const fs = require("fs");
 
-// const keys = require("./keys");
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
-if (process.argv[2]=== "movie-this") {
-    omdbapi();
+
+if (process.argv[2] === "movie-this") {
+    omdbApi();
+}
+else if (process.argv[2] === "my-tweets") {
+    twitterNPM();
+}
+else if (process.argv[2] === "spotify-this-song") {
+    spotifyNPM();
+}
+else if (process.argv[2] === "do-what-it-says") {
+    readRandom();
 }
 
+//Twitter section
+function twitterNPM() {
+    var params = { screen_name: 'Elizabe35444694', count: 20 };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            var tweetArray = tweets.map(function (tweet) {
+                return tweet.text
+            });
+            console.log(tweetArray.join("\n"))
+        }
+    });
+}
 //OMDB section
-function omdbapi() {
-    
+function omdbApi() {
+
     var nodeName = process.argv;
     var movieName = "";
     if (!nodeName[3]) {
         console.log(nodeName[3])
         movieName = "Mr.Nobody"
-    } 
+    }
     for (var i = 3; i < nodeName.length; i++) {
         if (i > 3 && i < nodeName.length) {
             movieName += "+" + nodeName[i];
         }
         else {
-            movieName +=nodeName[i];
+            movieName += nodeName[i];
         }
     }
 
@@ -39,4 +64,30 @@ function omdbapi() {
         }
     });
 }
+function spotifyNPM() {
+    var nodeMusic = process.argv;
+    var songName = "";
+    if (!nodeMusic[3]) {
+        songName = "The Sign, Ace of Base"
+    }
+    for (var i = 3; i < nodeMusic.length; i++) {
+        if (i > 3 && i < nodeMusic.length) {
+            songName += "+" + nodeMusic[i];
+        }
+        else {
+            songName += nodeMusic[i];
+        }
+    }
+    spotifyQuery.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        else {
+            var dataResult = data.tracks.items[0];
 
+            console.log("Artist: " + dataResult.artists[0].name + "\nSong Name: " + dataResult.name + "\nSpotify Link: " + dataResult.preview_url + "\nAlbum: " + dataResult.album.name);
+        };
+    });
+
+}
